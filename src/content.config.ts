77384b235d6be8004f40d1c00e3bd44531507032
mkki <devmkki@gmail.com>
2025-12-14 23:43:57 +0900

    fix: blog에서 posts로 네이밍 통일

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,8 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-const blog = defineCollection({
-  loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+const posts = defineCollection({
+  loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -11,4 +11,4 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+export const collections = { posts };
