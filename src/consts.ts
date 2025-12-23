907fd23a2628c263f0140a6ab4be256ac29f1491
mkki <devmkki@gmail.com>
2025-12-24 02:56:16 +0900

    fix: meta title, description 변경

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'devmkki';
-export const SITE_DESCRIPTION = 'Welcome to mkki\'s world!';
+export const SITE_TITLE = 'devmkki\'s blog';
+export const SITE_DESCRIPTION = '개발자 김민강의 기술 블로그';
